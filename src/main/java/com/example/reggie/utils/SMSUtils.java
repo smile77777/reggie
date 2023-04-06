e2519c22bc18074af5279176553e10bd5906c554
smile77777 <112932813+smile77777@users.noreply.github.com>
2023-04-06 15:33:51 +0800

    Update SMSUtils.java

--- src/main/java/com/example/reggie/utils/SMSUtils.java
+++ src/main/java/com/example/reggie/utils/SMSUtils.java
@@ -20,7 +20,7 @@ public class SMSUtils {
 	 * @param param 参数
 	 */
 	public static void sendMessage(String signName, String templateCode,String phoneNumbers,String param){
-		DefaultProfile profile = DefaultProfile.getProfile("cn-hangzhou", "LTAI5tBwPq4EbjTDc8BVj4NU", "YltLFz8THqbktNOtUrH2maSXVpPO9o");
+		DefaultProfile profile = DefaultProfile.getProfile("cn-hangzhou");
 		IAcsClient client = new DefaultAcsClient(profile);
 
 		SendSmsRequest request = new SendSmsRequest();
